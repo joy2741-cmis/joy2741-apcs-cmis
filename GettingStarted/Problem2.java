09eb86ff5d5fa308847c509bd6d5dc476678591d
cmis <cmis@cmis-hs06.(none)>
2016-08-09 11:50:34 +0700

    Working on it

---
+++ GettingStarted/Problem2.java
@@ -0,0 +1,15 @@
+import javax.swing.JOptionPane;
+
+public class Problem2
+{
+    public static void main( String[] args )
+    {
+        String input = JOptionPane.showInputDialog( "What is your name?");
+        
+        System.out.println( "Hello," + " " + input );
+        
+    }
+    
+}
+
+    
