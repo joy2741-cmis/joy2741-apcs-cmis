85cc18bbd757cd5ff22a96bc9b93831e0e0c6410
cmis <cmis@cmis-hs01.(none)>
2016-08-24 08:41:24 +0700

    ...

---
+++ Fundamentals/FundamentalsP8.java
@@ -0,0 +1,18 @@
+
+public class FundamentalsP8
+{
+   public static void main( String[] args )
+   {
+       String[] favArtist = { "Artist1", "Artist2", "Artist3" };
+       
+       for( int i = 0; i < favArtist.length; i++)
+       {
+           String Artist = favArtist[i];
+           System.out.println( Artist );
+        }
+       
+            System.out.println( "\n" + "\n" );
+            
+            
+    }
+}
