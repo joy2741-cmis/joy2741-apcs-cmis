ea703b1256be70e3893cdb35dd82d058161d2bfb
cmis <cmis@cmis-hs01.(none)>
2016-09-05 08:39:23 +0700

    Working on the 3rd problem

--- EulerProblems/Problem2.java
+++ EulerProblems/Problem2.java
@@ -4,54 +4,48 @@ public class Problem2
 {
    public static void main( String[] args )
    {
-       /*String sequence = "123";
-       char[] seqArray = sequence.toCharArray();*/
-       
-      
+            
        int a = 1;
        int b = 2;
        int i = a + b;
+       int runtime = 0;
+       int sum = 2;
+       
+        
             
-            while (i < 4000000)
+          while (sum < 4000000)
        {
-                   
-                for( a = 1; a < 4000000; a++ )
-                {
-                    System.out.println(a);
-                    a = b;
-                    System.out.println(a);
-                }
+                 
+           for(runtime = 0; runtime > 0; runtime++)
+           {
+               if (runtime == 1)
+                  {
+                     i = a + b;
+                     System.out.println(i);
+                  }
+           }    
+                
+                 a = b;
+                 b = i;
+                 i = a + b;
+                 
+                 if( i % 2 ==0)
+                 {
+                     sum = sum + i;
+                     //System.out.println(i);
+                    }
+                 
+                 
+                                                
+            }
+       
+            System.out.println(sum);
              
-                for( b = 2; b < 4000000; b++ )
-                {
-                    System.out.println(b);
-                    b = i;
-                    System.out.println(b);
-                }
-             
-           
+              
         }
        
-        /*
-         * a + b = i
-         * a then has the same value as b --> a2 = b1
-         * b then has the same value as i --> b2 = i1
-         * a2 + b2 = i2 
-         * 
-         * 
-         * 
-         * 
-         * 
-         * 
-         */
-       
-       
-       
-      
-       
-       
-       
     }
+
    
   
-}
+
