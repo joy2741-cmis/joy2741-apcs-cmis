312ea42a7b02a97f2881af85dcca759ed7b6c35c
cmis <cmis@cmis-hs01.(none)>
2016-09-23 09:56:16 +0700

    Getter/Setter

---
+++ ObjectOrientedProgram/FishTank.java
@@ -0,0 +1,85 @@
+
+public class FishTank
+{
+
+    private boolean fish; //true --> there's fish //false --> no fish
+    private int numberOfFish;
+    private String fishNumber = "#0"; 
+    private double tankHeight;
+    private boolean clean; //true --> clean //false --> dirty
+
+    public FishTank( boolean fish )
+    {
+        this.fish = fish;
+        this.numberOfFish = numberOfFish;
+        numberOfFish = (int)(Math.random()*10);
+
+        this.fishNumber = fishNumber;
+
+    }//end fishTank
+
+    
+    public boolean getFish(){
+        
+        if( fish )
+        {
+            fish = "";
+        }
+        else 
+        {
+            fish =  "The tank is empty";
+        }
+
+        return getFish;
+    }//end getter fish
+    
+        public void setfish( boolean newFish )
+        {
+            fish = newFish;
+    }// end setter fish
+
+    public int getNumberOfFish()
+    {
+        return numberOfFish;
+    }//end getter numberOfFish
+
+    public void setNumberOfFish( int newNumberOfFish )
+    {
+        numberOfFish = newNumberOfFish;
+    }//end setter numberOfFish
+
+    public String getFishNumber()
+    {
+        
+        return fishNumber;
+    }//end getter fishNumber
+
+    public void setFishNumber( String newFishNumber )
+    {
+        fishNumber = newFishNumber;
+    }//end setter fishNumber
+
+    public double getTankHeight()
+    {
+        return tankHeight;
+    }//end getter tankHeight
+
+    public void setTankHeight( double newTankHeight )
+    {
+        tankHeight = mewTankHeight;
+    }//end setter tankHeight
+    
+    public static void main( String[] args)
+    {
+        FishTank F1 = new FishTank(true);
+        System.out.println(F1);
+
+    }//end method
+
+    public String toString()
+    {
+        String output = "Fish? " + fish + "\nNumber of Fish: " + numberOfFish + "\nFish Name: " + fishNumber + "\nTank height: " + tankHeight + "\nClean? " + clean;
+        return output;
+    }
+}
+
---
+++ ObjectOrientedProgram/Person.java
@@ -0,0 +1,28 @@
+
+public class Person
+{
+    private double age;
+    private boolean gender;
+    private double year;
+    private double mLifeEx = 80.5;
+    private double fLifeEx = 86.8;
+
+    public Person( int age, int gender )
+    {
+        this.age = age;
+        //this.gender = gender;
+        this.year = year;
+        this.mLifeEx = mLifeEx;
+        this.mLifeEx = mLifeEx;
+    }//end Person
+    
+    public void setYear( double newYear )
+    {
+        
+    }
+    
+    public double year(){
+        
+    }//end getter year
+    
+}//end class Person
