10a199a66f13606d9a6143f91705e6a7d5487da5
cmis <cmis@cmis-hs00.(none)>
2016-10-12 11:55:21 +0700

    MUAHAHAHAHA DONE

---
+++ SIS/Course.java
@@ -0,0 +1,42 @@
+import javax.swing.JOptionPane;
+public class Course
+{
+    private String course;
+    private String[] courses;
+    private int index = 0;
+    
+    public Course()
+    {
+        this.course = JOptionPane.showInputDialog( "Type your course: " );
+    }//end constructor
+    
+    //Method name
+    
+    public String[] getCourses()
+    {
+        return courses;
+    }//end getNames
+    
+    public void setCourses( String[] newCourses )
+    {
+        courses = newCourses;
+    }//end setNames
+    
+    public String getCourse( int index )
+    {
+        return courses[index];
+    }//end getName
+    
+    public void addCourse( String course )
+    {
+        courses[ index ] = course;
+        index++;
+    }//end getName
+    
+    public String toString()
+    {
+        String output = "Course Name: " + course;
+        return output;
+    }
+    
+}
--- SIS/SISApp.java
+++ SIS/SISApp.java
@@ -3,17 +3,21 @@ public class SISApp
 {
 
     public static void main(String [] arg){
+
         int output = (Integer.parseInt(JOptionPane.showInputDialog( "1: View Student List" + "\n2: View Course List" + "\n3: Add a Student" + "\n4: Add a Course" + "\n0: Quit")));
         //Name[] names = new Name[3];
         //Grade[] grades = new Grade[3];
         Name[] names = new Name[1000];
         Grade[] grades = new Grade[1000];
-        int i = 0;
+        Course[] courses = new Course[1000];
+        int index = 0;
+        int index2 = 0;
+
         while( output > 0 )
         {
             if( output == 1 ) //View Student List
             {
-                for( i = 0; i < names.length; i++ )
+                for( int i = 0; i < names.length; i++ )
                 {
                     if( names[i] != null )
                     {
@@ -22,34 +26,54 @@ public class SISApp
                     }
                     else
                     {                        
-                        i = 1001;                       
+                        i = names.length+1;                       
                     }
                 }
                 output = (Integer.parseInt(JOptionPane.showInputDialog( "1: View Student List" + "\n2: View Course List" + "\n3: Add a Student" + "\n4: Add a Course" + "\n0: Quit")));  
             }
             else if( output == 2 ) //View Course List
             {
+                for( int i = 0; i < courses.length; i++ )
+                {
+                    if( courses[i] != null )
+                    {
+                        System.out.println(courses[i]);
+                    }
+                    else
+                    {                        
+                        i = courses.length+1;                       
+                    }
+                }
                 output = (Integer.parseInt(JOptionPane.showInputDialog( "1: View Student List" + "\n2: View Course List" + "\n3: Add a Student" + "\n4: Add a Course" + "\n0: Quit")));
             }
             else if( output == 3 ) //Add a Student
             {
-                for( int start = 0; start < 1 && i < names.length; start++ )
+                for( int start = 0; start < 1 && index < names.length; start++ )
                 {
-                    names[i] = new Name();
-                    grades[i] = new Grade();   
-                    i++;
+                    names[index] = new Name();
+                    grades[index] = new Grade();   
+                    index++;
                 }
                 output = (Integer.parseInt(JOptionPane.showInputDialog( "1: View Student List" + "\n2: View Course List" + "\n3: Add a Student" + "\n4: Add a Course" + "\n0: Quit")));
             }
             else if( output == 4 ) //Add a Course
             {
+                for( int start = 0; start < 1 && index2 < courses.length; start++ )
+                {
+                    courses[index2] = new Course();
+                    index2++;
+                }
                 output = (Integer.parseInt(JOptionPane.showInputDialog( "1: View Student List" + "\n2: View Course List" + "\n3: Add a Student" + "\n4: Add a Course" + "\n0: Quit")));
             }
             else if( output == 0 ) //Quit
             {
                 return;
+            }
+            else
+            {
+                output = (Integer.parseInt(JOptionPane.showInputDialog( "1: View Student List" + "\n2: View Course List" + "\n3: Add a Student" + "\n4: Add a Course" + "\n0: Quit")));
             }//end if
         }//end while
-    }//end main
-}//end class
+    }	
+}//end main
 
