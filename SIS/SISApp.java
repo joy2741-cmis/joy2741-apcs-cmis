52850f35056575dd6c261f9501c397964d299088
cmis <cmis@cmis-hs00.(none)>
2016-10-13 14:39:21 +0700

    .

--- SIS/SISApp.java
+++ SIS/SISApp.java
@@ -1,18 +1,16 @@
 import javax.swing.JOptionPane;
 public class SISApp
 {
-
     public static void main(String [] arg){
 
         int output = (Integer.parseInt(JOptionPane.showInputDialog( "1: View Student List" + "\n2: View Course List" + "\n3: Add a Student" + "\n4: Add a Course" + "\n0: Quit")));
-        //Name[] names = new Name[3];
-        //Grade[] grades = new Grade[3];
         Name[] names = new Name[1000];
         Grade[] grades = new Grade[1000];
         Course[] courses = new Course[1000];
         int index = 0;
         int index2 = 0;
-
+        String nameList = "";
+        int outOne;
         while( output > 0 )
         {
             if( output == 1 ) //View Student List
@@ -21,15 +19,24 @@ public class SISApp
                 {
                     if( names[i] != null )
                     {
-                        System.out.println(names[i]);
-                        System.out.println(grades[i]);
+                        //System.out.println(names[i]);
+                        //System.out.println(grades[i]);                        
                     }
                     else
                     {                        
                         i = names.length+1;                       
                     }
                 }
-                output = (Integer.parseInt(JOptionPane.showInputDialog( "1: View Student List" + "\n2: View Course List" + "\n3: Add a Student" + "\n4: Add a Course" + "\n0: Quit")));  
+                //output = ((Integer.parseInt(JOptionPane.showInputDialog ( "1: View Student List" + "\n2: View Course List" + "\n3: Add a Student" + "\n4: Add a Course" + "\n0: Quit"))); 
+                outOne = (Integer.parseInt(JOptionPane.showInputDialog ( "list of student to pick Name blah blah blah" + "\n1: add course"+ "\n2:change grade" + "\n0: Quit")));  
+                    if(outOne == 1)
+                    {
+                        System.out.println("fghjkl");
+                    }
+                    if(outOne == 0)
+                    {
+                        output = (Integer.parseInt(JOptionPane.showInputDialog( "1: View Student List" + "\n2: View Course List" + "\n3: Add a Student" + "\n4: Add a Course" + "\n0: Quit")));
+                    }
             }
             else if( output == 2 ) //View Course List
             {
@@ -74,6 +81,6 @@ public class SISApp
                 output = (Integer.parseInt(JOptionPane.showInputDialog( "1: View Student List" + "\n2: View Course List" + "\n3: Add a Student" + "\n4: Add a Course" + "\n0: Quit")));
             }//end if
         }//end while
-    }	
+    }   
 }//end main
 
