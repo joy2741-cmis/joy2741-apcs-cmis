55d5fa69f96621eb83726e1968988845e70e6708
cmis <cmis@cmis-hs00.(none)>
2016-10-17 09:20:45 +0700

    .

--- SIS/SISApp.java
+++ SIS/SISApp.java
@@ -11,33 +11,52 @@ public class SISApp
         int index2 = 0;
         String nameList = "";
         int outOne;
+        String SNL = "";
         while( output > 0 )
         {
-            if( output == 1 ) //View Student List
+            if( output == 3 ) //Add a Student
+            {
+                for( int start = 0; start < 1 && index < names.length; start++ )
+                {
+                    names[index] = new Name();
+                    grades[index] = new Grade();   
+                    index++;
+                    SNL += names[index];
+                    SNL += grades[index];
+                }
+                output = (Integer.parseInt(JOptionPane.showInputDialog( "1: View Student List" + "\n2: View Course List" + "\n3: Add a Student" + "\n4: Add a Course" + "\n0: Quit")));
+            }
+            else if( output == 1 ) //View Student List
             {
                 for( int i = 0; i < names.length; i++ )
                 {
                     if( names[i] != null )
                     {
-                        //System.out.println(names[i]);
-                        //System.out.println(grades[i]);                        
+                        System.out.println(names[i]);
+                        System.out.println(grades[i]);                        
                     }
                     else
                     {                        
                         i = names.length+1;                       
                     }
                 }
-                //output = ((Integer.parseInt(JOptionPane.showInputDialog ( "1: View Student List" + "\n2: View Course List" + "\n3: Add a Student" + "\n4: Add a Course" + "\n0: Quit"))); 
-                outOne = (Integer.parseInt(JOptionPane.showInputDialog ( "list of student to pick Name blah blah blah" + "\n1: add course"+ "\n2:change grade" + "\n0: Quit")));  
+                //output = ((Integer.parseInt(JOptionPane.showInputDialog ( "1: View Student List" + "\n2: View Course List" + "\n3: Add a Student" + "\n4: Add a Course" + "\n0: Quit")))); 
+                for (int idx =0; idx < names.length; idx++){
+                    outOne = (Integer.parseInt(JOptionPane.showInputDialog ( "list of student to pick" + names[idx]+ grades[idx]  + "\n1: add course"+ "\n2:change grade" + "\n0: Quit")));
+                    
                     if(outOne == 1)
-                    {
-                        System.out.println("fghjkl");
+                    { 
+                        output = 4;
                     }
                     if(outOne == 0)
                     {
-                        output = (Integer.parseInt(JOptionPane.showInputDialog( "1: View Student List" + "\n2: View Course List" + "\n3: Add a Student" + "\n4: Add a Course" + "\n0: Quit")));
+                        idx = names.length+1;
+                        output = ((Integer.parseInt(JOptionPane.showInputDialog ("1: View Student List" + "\n2: View Course List" + "\n3: Add a Student" + "\n4: Add a Course" + "\n0: Quit")))); 
                     }
+                }
+
             }
+
             else if( output == 2 ) //View Course List
             {
                 for( int i = 0; i < courses.length; i++ )
@@ -53,16 +72,7 @@ public class SISApp
                 }
                 output = (Integer.parseInt(JOptionPane.showInputDialog( "1: View Student List" + "\n2: View Course List" + "\n3: Add a Student" + "\n4: Add a Course" + "\n0: Quit")));
             }
-            else if( output == 3 ) //Add a Student
-            {
-                for( int start = 0; start < 1 && index < names.length; start++ )
-                {
-                    names[index] = new Name();
-                    grades[index] = new Grade();   
-                    index++;
-                }
-                output = (Integer.parseInt(JOptionPane.showInputDialog( "1: View Student List" + "\n2: View Course List" + "\n3: Add a Student" + "\n4: Add a Course" + "\n0: Quit")));
-            }
+
             else if( output == 4 ) //Add a Course
             {
                 for( int start = 0; start < 1 && index2 < courses.length; start++ )
