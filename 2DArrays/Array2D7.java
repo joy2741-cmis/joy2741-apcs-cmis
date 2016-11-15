9e003963246f2ae39fa9a9a02cc21a28b27edff5
cmis <cmis@cmis-hs01.(none)>
2016-11-15 08:40:23 +0700

    Found a problem in 7

--- 2DArrays/Array2D7.java
+++ 2DArrays/Array2D7.java
@@ -11,20 +11,21 @@ public class Array2D7
 
         boolean correctRow = false;
         boolean correctColumn = false;
+        
 
         for( int x = 0; x < gameBoard.length; x++ )
         {
             for( int y = 0; y < gameBoard[0].length; y++ )
             {
                 gameBoard[x][y] = ((int)(Math.random()*99)+1);
-                //System.out.print("X   ");
+                System.out.print("X   ");
                 gameBoard[randomRow][randomColumn] = 0;
-                System.out.print(gameBoard[x][y] + "   ");
+                //System.out.print(gameBoard[x][y] + "   ");
             }//end inner for
             System.out.println("\n");
         }//end outer for                   
         
-        System.out.println("Zero: " + (randomRow + 1)  + ", " + (randomColumn + 1));
+        //System.out.println("Zero: " + (randomRow + 1)  + ", " + (randomColumn + 1));
         
         int rowZero = Integer.parseInt(JOptionPane.showInputDialog("Guess the row"));
         int columnZero = Integer.parseInt(JOptionPane.showInputDialog("Guess the column"));
