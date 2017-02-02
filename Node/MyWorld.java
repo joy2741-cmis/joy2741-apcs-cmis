aa76b4cf64a6375788a7c64b191780e19ba52b16
cmis <cmis@cmis-hs01.(none)>
2017-02-02 11:56:29 +0700

    .

---
+++ Node/MyWorld.java
@@ -0,0 +1,87 @@
+import greenfoot.*;  
+import java.util.ArrayList;
+
+public class MyWorld extends World
+{
+    private Node node, first, last;
+    ArrayList<Node> nodes = new ArrayList<Node>();
+    public MyWorld()
+    {
+        super(600, 400, 1); 
+        node = new Node(null);
+        first = node;
+        addObject(node, 300, 200);
+        System.out.println("here");
+        for (int j = 1; j < 10; j++){
+            node = new Node(node);
+            addObject(node, 300, 200);
+            last = node;
+            nodes.add(node);
+        }
+
+    }
+
+    public void act(){
+        String key = Greenfoot.getKey();
+        if (key != null){
+            if ( key.equals("n")){
+                addNode();
+            }
+
+            if ( key.equals("r")){
+                removeNode();
+            }
+
+            if( key.equals("l")){
+                removeLastNode();
+            }
+        }
+    }
+
+    public int[] getNewLocation(){
+        int[] location;
+        if(getObjects(null).contains(first)){
+            location = new int[]{first.getX(), first.getY()};
+        }else{
+            location = new int[]{Greenfoot.getRandomNumber(getWidth()), Greenfoot.getRandomNumber(getHeight())};
+        }
+        return location;
+    }
+
+    public void addNode(){
+        node = new Node(last);
+        if(first == null){
+            first = node;
+        }
+        int[] loc = getNewLocation();
+        last = node;
+        addObject(node, loc[0], loc[1]);
+        nodes.add(node);
+    }
+
+    public void removeNode(){
+        if ( last != null ){
+            removeObject(last);
+            last = last.getTarget();
+            first = last == null ? null : first;
+            nodes.remove(node);
+        }
+    }
+
+    public void removeLastNode(){
+        if ( first != null ){
+            removeObject(first);
+            nodes.remove(node);
+        }
+        for( int i = 0; i < nodes.size(); i++ )
+        {
+            Node node = nodes.get(i);
+            if( node != null )
+            {
+                node = first;
+                i = nodes.size() +1;
+            }
+        }//end for
+    }
+
+}
