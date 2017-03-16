e98bdef8082f19f4af9d0fe8c129717826a4712c
cmis <cmis@cmis-hs01.(none)>
2017-03-16 09:55:15 +0700

    .

---
+++ RatAttack-problem/MyWorld.java
@@ -0,0 +1,54 @@
+import greenfoot.*; 
+import java.util.*;
+public class MyWorld extends World
+{
+    private long startTime;
+    private long runningTime;
+    private int level = 1;
+    private int score = 0;
+    public MyWorld()
+    {
+        super(600, 400, 1); 
+        init(level);
+    }
+
+    public void init(int level){
+        List<Actor> actors = getObjects(null); //empty ArrayList
+        for(Actor a: actors){// (int a = 0; a < actors.length; a++)
+            removeObject(a); //remove previous actors
+        }
+        for(int i=0; i<level; i++){
+            addObject(new Rat(), 50, Greenfoot.getRandomNumber(400));
+            //add rat according to lvl
+        }
+        addObject(new Exterminator(), 520, 200);//add exterminator
+        addObject(new Trap(), 550, 200);//add trap(skull)
+        startTime = System.currentTimeMillis();//get current time
+
+    }
+
+    public void act(){
+        runningTime = System.currentTimeMillis() - startTime;
+        //running time = current time - time at the start of the game
+        if(getObjects(Rat.class).size() == 0){
+            init(level++);//when there's no more rat, level up
+        }
+        showText(score+"  "+(int)(15 -(runningTime/1000.0)), 50, 50);
+        //15 is the limit
+        //runningTime/1000 convert millisecond to second
+    }
+
+    public void addCheese(int x, int y){
+        addObject(new Cheese(), x, y);//add cheese
+    }
+
+
+    public void score(){
+        score++;//add score
+    }
+
+    public void lose(){
+        showText("Game Over!!", 300, 200);//game over
+        Greenfoot.stop();//game stops
+    }
+}
---
+++ RatAttack-problem/Rat.java
@@ -0,0 +1,102 @@
+import greenfoot.*;
+import java.util.*;
+public class Rat extends Actor
+{
+    private int direction;
+    private int eaten;
+    public Rat(){
+        direction = Greenfoot.getRandomNumber(4);
+        getImage().scale(50,25);
+        eaten = 0; //how many cheese
+    }
+
+    public void act() 
+    {
+        eat();
+        Cheese closest = getTargetCheese(200); 
+        //check for cheese within the range(200)
+        if(closest == null){
+            wander();
+            //if there's no cheese, move randomly
+        } else{
+            moveTowardCheese(closest);
+            //else, move towards the closest cheese
+        }
+        die();//if touches trap, dies
+    }   
+
+    public void wander(){ //move randomly
+        if(Math.random() > 0.95){
+            //Math.random() = 0.0-1.0 //if more than 0.95
+            direction = Greenfoot.getRandomNumber(4);
+            //get 0-3
+        }
+        int dx = 0;
+        int dy = 0;
+        if ( direction == 0 ){ //up
+            dy = -1;
+        } else if ( direction == 1 ){ //down
+            dy = 1;
+        } else if ( direction == 2 ){ //left
+            dx = -1;
+        } else if ( direction == 3 ){ //right
+            dx = 1;
+        }
+
+        setLocation(getX() + dx, getY() + dy); //change direction
+    }
+
+    public void moveTowardCheese(Cheese cheese){
+        int dx = 0;
+        int dy = 0;
+        int cheeseX = cheese.getX();
+        int cheeseY = cheese.getY();
+        int x = getX();
+        int y = getY();
+        
+        if ( x < cheeseX ){//if cheese is to the right, move right
+            dx = 1;
+        }else if ( x > cheeseX){//if cheese is to the left, move left
+            dx = -1;
+        }
+        
+        if ( y < cheeseY ){//if cheese is down, move down
+            dy = 1;
+        }else if ( y > cheeseY ){//if cheese is up, move up
+            dy = -1;
+        }
+        setLocation(x + dx, y + dy);//move
+    }
+    
+    public Cheese getTargetCheese(int range){ //check for cheese to pick up
+        Cheese target = null; //set target to none
+        List<Cheese> cheeses = getObjectsInRange(range, Cheese.class);
+        //if cheese is within range(200), add cheese to arraylist
+        if (cheeses.size() == 0){
+            target = null;
+            //if there's no cheese in the array, no target
+        }else if( cheeses.size() >= 1 ){
+            target = cheeses.get(0);
+            //if there's cheese in the array, get the first one
+        } 
+        return target; //return no cheese or the closet cheese
+    }
+    
+    public void eat(){
+        Cheese cheese = (Cheese)getOneIntersectingObject(Cheese.class);
+        if( cheese != null){
+            World w = getWorld();
+            w.removeObject(cheese);
+            eaten++;
+        }
+    }
+    
+    
+    public void die(){
+        if(isTouching(Trap.class)){
+            MyWorld w = (MyWorld)getWorld();
+            w.removeObject(this);
+            w.score();
+        }
+    }
+}
