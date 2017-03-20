8d8cea6120fd759a708b2a6396b136dc3c37c3e9
joy2741-cmis <joy2741@cmis.ac.th>
2017-03-20 08:03:47 +0700

    Update CheeseBomb.java

--- RatAttack-problem/CheeseBomb.java
+++ RatAttack-problem/CheeseBomb.java
@@ -3,7 +3,7 @@ import java.awt.Color;
 
 public class CheeseBomb extends Actor
 {
-    private int timer = 1000;
+    private int timer = 1000; 
     private GreenfootImage img;
     public CheeseBomb(){
         img = new GreenfootImage(25,25);
@@ -14,7 +14,7 @@ public class CheeseBomb extends Actor
     }
     public void act() 
     { 
-        if ( timer-- < 0){
+        if ( timer-- < 0){ //if timer decrements until less than 0, remove cheesebomb
             World w = getWorld();
             w.removeObject(this);
         }
