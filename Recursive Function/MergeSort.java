898be3c18bd0b3ea10297a34b43db7336e0777b4
cmis <cmis@cmis-hs10.(none)>
2017-03-23 09:40:40 +0700

    .

--- Recursive Function/MergeSort.java
+++ Recursive Function/MergeSort.java
@@ -4,38 +4,17 @@ public class MergeSort
     public static void main( String[] args )
     {       
         int[] nums = new int[] { 3, 7, 1, 9, 5, 8, 6, 2, 4, 0, 0, 99 };
-        int[] right = new int[nums.length/2];        
-        int[] leftE = new int[nums.length/2]; //even
-        int[] leftO = new int[(nums.length/2)+1];
-
-        if( nums.length % 2 != 0 ) 
-        {
-            split(nums,leftO,right);//odd            
-            for( int i = 0; i < leftO.length; i++ )
-            {
-                System.out.print( leftO[i] + " ");
-            }//end for
-        }
-        else
-        {
-            split(nums,leftE,right);//even
-            for( int i = 0; i < leftE.length; i++ )
-            {
-                System.out.print( leftE[i] + " ");
-            }//end for
-        }//end if-else
-
+        int[] placements = new int[nums.length];
+        
     }//end method
 
-    public static int[] split( int[] nums, int[] left, int[] right )
+    public static int[] split( int[] nums, int[] placements )
     {
-        int i = 0;
-        for( int x = 0; x < left.length; x++, i++ )
+        for( int i = 1; i < nums.length; i++ )//run through nums
         {
-            left[x] = nums[i];
+            
         }//end for
-        return left;
-
+        return nums;
     }//end split
 
     public static int[] sort( int[] nums )
