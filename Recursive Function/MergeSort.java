9079c7533089869128e43b2d764cfc49c8b7cf31
cmis <cmis@cmis-hs11.(none)>
2017-03-29 09:20:36 +0700

    .

--- Recursive Function/MergeSort.java
+++ Recursive Function/MergeSort.java
@@ -3,35 +3,47 @@ public class MergeSort
 {
     public static void main( String[] args )
     {       
-        int[] nums = new int[] { 3, 7, 1, 9, 5, 8, 6, 2, 4, 0, 0, 99, 7 };
-        int[] placements = new int[nums.length];
-        split(nums);
+        int[] nums = new int[] { 3, 7, 1, 9, 5, 8, 6, 2, 4, 0, 0, 99, 7 };                        
+        int[] placementsL = Arrays.copyOf(nums, nums.length);
+        int[] placementsR = Arrays.copyOf(nums, nums.length);
+        split(placementsL, placementsR);
     }//end method
 
-    public static int[] split( int[] nums )
+    public static int[][] split( int[] placementsL, int[] placementsR )
     {
         int counter = 0;
-        int[] left = Arrays.copyOf(nums, nums.length/2);
-        int[] right = Arrays.copyOfRange(nums, nums.length/2, nums.length);
-
-        for( int i = 0; i < numsCopied.length; i++ )//run through nums
+        int[] left = Arrays.copyOf(placementsL, placementsL.length/2);
+        int[] right = Arrays.copyOfRange(placementsR, placementsR.length/2, placementsR.length);
+        int[][] result = new int[1][1];
+        for( int i = 0; i < left.length; i++ )
         {
-            System.out.print(numsCopied[i] + " ");
+            System.out.print(left[i] + " ");
         }//end for
-        System.out.println();
-        return nums;
+        System.out.println("Right\n");
+        for( int i = 0; i < right.length; i++ )
+        {
+            System.out.print(right[i] + " ");
+        }//end for
+
+        placementsL = Arrays.copyOf(left, left.length);
+        placementsR = Arrays.copyOf(right, right.length);
+
+        if( left.length == 1 )
+        {
+            for( int i = 0; i < result.length; i++ )
+            {
+                for( int j = 0; j < result[0].length; j++ )
+                {
+                    result[1][0] = left;
+                }//end j
+            }//end i
+            return left;
+        }
+        else
+        {
+            return split(right);
+        }
     }//end split
-
-    public static int[] sort( int[] nums )
-    {        
-        return nums;
-    }//end sort
-
-    public static int[] merge( int[] nums )
-    {
-        return nums;
-    }//end merge
-
 }//end MergeSort
 
 //int[] RArray = Arrays.copyOf(nums, 1);
